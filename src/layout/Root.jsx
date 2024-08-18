0558586b99dc15eddaf944cbb58fffe730d56aea
AhnafTahmiddie <ahnaftahmid2604@gmail.com>
2024-08-18 17:31:08 +0600

    footer added

--- src/layout/Root.jsx
+++ src/layout/Root.jsx
@@ -1,13 +1,13 @@
 import { Outlet } from "react-router-dom";
 import NavigationBar from "../Pages/Shared/NavigationBar/NavigationBar";
-// import Footer from "../Pages/Shared/Footer/Footer";
+import Footer from "../Pages/Shared/FooterComp/FooterComp";
 
 const Root = () => {
     return (
         <div>
             <NavigationBar></NavigationBar>
             <Outlet></Outlet>
-            {/* <Footer></Footer> */}
+            <Footer></Footer>
         </div>
     );
 };
